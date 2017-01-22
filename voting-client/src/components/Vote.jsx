aa522575e8226be482ad6ce96c3b25f58a0193f9
pablodicosta <pablo.diaz.acosta@gmail.com>
2017-01-22 05:25:31 -0300

    Added middleware for server communication

--- voting-client/src/components/Vote.jsx
+++ voting-client/src/components/Vote.jsx
@@ -17,8 +17,8 @@ export default React.createClass({
       <div className="voting">
         {this.getPair().map(entry =>
           <button key={entry}
-                  disabled={this.isDisabled()}
-                  onClick={() => this.props.vote(entry)}>
+            disabled={this.isDisabled()}
+            onClick={() => this.props.vote(entry)}>
             <h1>{entry}</h1>
             {this.hasVotedFor(entry) ?
               <div className="label">Voted</div> :
